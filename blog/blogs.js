19ac15467c04a6d62eb96b06dbcb48c3c268472f
Quicktoolspk <jibranahmadteam2022@gmail.com>
2026-08-25 01:59:47 +0500

    Update blogs.js

--- blog/blogs.js
+++ blog/blogs.js
@@ -1,1 +1,10 @@
-
+const blogs = [
+    {
+        title: "Summer Fashion Trends 2026",
+        date: "August 25, 2026",
+        category: "Fashion",
+        image: "assets/images/blog-placeholder.jpg",
+        description: "Discover the latest summer fashion trends and stylish clothing ideas for 2026.",
+        url: "blog/posts/summer-fashion-trends-2026.html"
+    }
+];
